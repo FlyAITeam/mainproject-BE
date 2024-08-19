c7a2986d0782f708f517e1b5f957e3206b7ab11d
SeungminKimdev <ksm7250432@gmail.com>
2024-08-19 14:24:42 +0900

    feat: 환경변수 설정

--- database.py
+++ database.py
@@ -3,14 +3,20 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
+from urllib.parse import quote_plus
 
-load_dotenv(override=True)
-db_name = os.getenv("DB_NAME")
-db_user = os.getenv("DB_USER")
-db_password = os.getenv("DB_PASSWORD")
-db_host = os.getenv("DB_HOST")
+if os.getenv("WEBSITE_HOSTNAME"):
+    env_connection_string = os.getenv("AZURE_POSTGRESQL_CONNECTIONSTRING")
+    details = dict(item.split('=') for item in env_connection_string.split())
+    SQLALCHEMY_DATABASE_URL = f"postgresql://{quote_plus(details['user'])}:{quote_plus(details['password'])}@{details['host']}:{details['port']}/{details['dbname']}"
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{db_user}:{db_password}@{db_host}:5432/{db_name}"
+else:
+    load_dotenv(override=True)
+    db_name = os.getenv("DB_NAME")
+    db_user = os.getenv("DB_USER")
+    db_password = os.getenv("DB_PASSWORD")
+    db_host = os.getenv("DB_HOST")
+    SQLALCHEMY_DATABASE_URL = f"postgresql://{db_user}:{db_password}@{db_host}:5432/{db_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
