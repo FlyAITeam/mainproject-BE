4a627b28e036506ad24ad74ca657fa5f521d7676
SeungminKimdev <ksm7250432@gmail.com>
2024-08-19 14:53:53 +0900

    feat: 파일 구성 변경

--- database.py
+++ database.py
@@ -1,22 +1,18 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
-from dotenv import load_dotenv
-from urllib.parse import quote_plus
 
-if os.getenv("WEBSITE_HOSTNAME"):
-    env_connection_string = os.getenv("AZURE_POSTGRESQL_CONNECTIONSTRING")
-    details = dict(item.split('=') for item in env_connection_string.split())
-    SQLALCHEMY_DATABASE_URL = f"postgresql://{quote_plus(details['user'])}:{quote_plus(details['password'])}@{details['host']}:{details['port']}/{details['dbname']}"
+if os.getenv("ENVIRONMENT") != "production":
+    from dotenv import load_dotenv
+    load_dotenv()
 
-else:
-    load_dotenv(override=True)
-    db_name = os.getenv("DB_NAME")
-    db_user = os.getenv("DB_USER")
-    db_password = os.getenv("DB_PASSWORD")
-    db_host = os.getenv("DB_HOST")
-    SQLALCHEMY_DATABASE_URL = f"postgresql://{db_user}:{db_password}@{db_host}:5432/{db_name}"
+db_name = os.getenv("DB_NAME")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_host = os.getenv("DB_HOST")
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{db_user}:{db_password}@{db_host}:5432/{db_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
