9e4291d0845c4fdd118b15423b116ed48fe83798
김동욱 <bradl0809@naver.com>
2024-08-20 10:47:44 +0900

    fix: init.sql 수정

--- init.sql
+++ init.sql
@@ -2,11 +2,20 @@
 
 -- 사용자 생성
 -- CREATE USER DB_USER WITH PASSWORD DB_PASSWORD;
-CREATE USER pelikan WITH PASSWORD '1234';
+DO $$
+BEGIN
+    -- 사용자가 존재하는지 확인
+    IF NOT EXISTS (
+        SELECT FROM pg_catalog.pg_roles 
+        WHERE rolname = 'pelikan_test') THEN
+        -- 사용자가 존재하지 않으면 생성
+        CREATE USER pelikan_test WITH PASSWORD '1234';
+        -- 데이터베이스를 생성 (데이터베이스 생성은 이 구문 밖에서 진행)
+        -- 사용자에게 슈퍼유저 권한 부여
+        ALTER ROLE pelikan_test SUPERUSER;
+    END IF;
+END$$;
 
 -- 새 데이터베이스 생성
 -- CREATE DATABASE DB_NAME;
 CREATE DATABASE test;
-
--- 생성한 사용자에게 새 데이터베이스의 모든 권한 부여
-ALTER ROLE pelikan SUPERUSER;
